076ec251d998996bf6aa0a20356baeb9371fd8de
Richard <55552821+Em1tt@users.noreply.github.com>
2023-01-28 20:12:06 +0100

    .

--- src/lib/Loader.svelte
+++ src/lib/Loader.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
-    let loading: boolean = true;
-    onMount(()=>{
-        localStorage.setItem("visited", "11 bytes?");
-    });
+    import { Rainbow } from 'svelte-loading-spinners';
+    let showIntro = true;
+    setTimeout(()=>{
+        showIntro = false;
+    },4000);
 </script>
 
-{#if loading}
-<div transition:fade="{{duration: 200}}" class="text-slate-200 fixed w-screen h-screen bg-slate-900 grid place-items-center transition-opacity duration-200">
+{#if showIntro}
+<div on:load transition:fade="{{duration: 200}}" class="text-slate-200 fixed w-screen h-screen bg-slate-900 grid place-items-center transition-opacity duration-200 top-0 left-0 hover:cursor-default selection:bg-transparent">
     <div class="flex flex-col flex-nowrap gap-2 animate-leaveTop relative">
         <span class="flex items-center gap-4">
             <h1 class="text-6xl font-semibold text-slate-400 animate-glow relative">emit</h1>
             <sub class="text-xl opacity-0 animate-enterRight relative">[ ih-<span class="font-semibold">mit</span>]</sub>
         </span>
         <p class="text-l animate-enterBottom relative opacity-0">to send forth (liquid, light, heat, sound, particles, etc.); discharge.</p>
+        <div class="flex justify-center mt-20">
+            <Rainbow size="40" color="#334155" unit="px" duration="2s" />
+        </div>
     </div>
 </div>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
     import Loader from "../lib/Loader.svelte";
 </script>
+<main class="grid place-items-center h-screen grid-cols-2 px-40">
+    <img src="/waves.svg" alt="">
+    <div>
+        <h1 class="text-slate-400 text-5xl font-semibold">Hello</h1>
+        <h2 class="text-slate-500 text-2xl max-w-lg">I'm Richard, a graphic designer and full-stack developer based in Slovakia</h2>
+    </div>
+</main>
 
+<!--
 <Loader/>
+-->
+<style></style>
