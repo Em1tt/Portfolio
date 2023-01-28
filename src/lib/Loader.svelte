54852063fcefe89b2a590006a993d050396a1d82
Richard <55552821+Em1tt@users.noreply.github.com>
2023-01-28 11:30:48 +0100

    setup

---
+++ src/lib/Loader.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+    let loading: boolean = true;
+    onMount(()=>{
+        localStorage.setItem("visited", "11 bytes?");
+    });
+</script>
+
+{#if loading}
+<div transition:fade="{{duration: 200}}" class="text-slate-200 fixed w-screen h-screen bg-slate-900 grid place-items-center transition-opacity duration-200">
+    <div class="flex flex-col flex-nowrap gap-2 animate-leaveTop relative">
+        <span class="flex items-center gap-4">
+            <h1 class="text-6xl font-semibold text-slate-400 animate-glow relative">emit</h1>
+            <sub class="text-xl opacity-0 animate-enterRight relative">[ ih-<span class="font-semibold">mit</span>]</sub>
+        </span>
+        <p class="text-l animate-enterBottom relative opacity-0">to send forth (liquid, light, heat, sound, particles, etc.); discharge.</p>
+    </div>
+</div>
+{/if}
---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      keyframes: {
+        glow: {
+          '50%': { filter: "brightness(1.4)", ["text-shadow"]: "0 0 10px rgba(255,255,255,0.3), 0 0 100px rgba(255,255,255,0.6), 0 0 1000px rgba(255,255,255,0.9)" },
+        },
+        enterLeft: {
+          '0%': {opacity:0, left:"-20px"},
+          '100%': { opacity: 100, left: 0 }
+        },
+        enterRight: {
+          '0%': {opacity:0, right:"-20px"},
+          '100%': { opacity: 100, right: 0 }
+        },
+        enterBottom: {
+          '0%': {opacity:0, bottom:"-20px"},
+          '100%': { opacity: 100, bottom: 0 }
+        },
+        leaveTop: {
+          '0%': {opacity:100, top: 0},
+          '100%': { opacity: 0, top: "-20px" }
+        }
+      },
+      animation: {
+        glow: "enterLeft 0.5s ease forwards, glow 2s ease 1s forwards",
+        enterLeft: "enterLeft 1s ease forwards",
+        enterRight: "enterRight 1s ease 0.5s forwards",
+        enterBottom: "enterBottom 1s ease 1s forwards",
+        leaveTop: "leaveTop 1s ease 3s forwards",
+      }
+    },
+  },
+
+  darkMode: "class",
+
+  plugins: [],
+}
